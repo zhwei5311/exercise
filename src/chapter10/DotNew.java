8d3282d532ffd8da30b1ca7788438b945b230161
zhwei <zhwei1228@qq.com>
2019-03-11 16:01:02 +0800

    add chapter 10 to github

---
+++ src/chapter10/DotNew.java
@@ -0,0 +1,14 @@
+package chapter10;
+
+public class DotNew {
+	public class Inner {
+		public void print() {
+			System.out.println("Inner");
+		}
+	}
+	public static void main(String[] args) {
+		DotNew dn = new DotNew();
+		DotNew.Inner dni = dn.new Inner();
+		dni.print();
+	}
+}
