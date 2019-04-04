f13af9d065ced2ecfbabb590d7e96a9ff704905c
zhwei5311 <zhwei1228@qq.com>
2019-04-04 21:58:58 +0800

    ChannelCopy file

---
+++ src/chapter18/ChannelCopy.java
@@ -0,0 +1,26 @@
+package chapter18;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelCopy {
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        if(args.length != 2) {
+            System.err.println("arguments : sourcefile destfile");
+            System.exit(1);
+        }
+        FileChannel in = new FileInputStream(args[0]).getChannel(),
+                out = new FileOutputStream(args[1]).getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(BSIZE);
+        while(in.read(buffer) != -1) {
+            buffer.flip();
+            out.write(buffer);
+            buffer.clear();
+        }
+    }
+}
